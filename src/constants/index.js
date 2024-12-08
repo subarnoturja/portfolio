63077b2b9a60ed1754953080e58d6aa0eb43f81e
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-09 01:49:24 +0600

    updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,7 @@
 import project1 from "../assets/projects/project-1.webp";
 import project2 from "../assets/projects/project-2.webp";
 import project3 from "../assets/projects/project-3.webp";
-import project4 from "../assets/projects/project-4.webp";
+import project4 from "../assets/projects/project-3.webp";
 
 export const PROJECTS = [
   {
@@ -9,28 +9,40 @@ export const PROJECTS = [
     image: project1,
     description:
       "A fully functional book selling website with features like book listing, seller dashboard, and user authentication.",
-    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+    technologies: ["React", "TailwindCSS", "Node.js", "MongoDB", "Firebase", "Express.js"],
+    liveLink: "https://storestacks.com",
+    clientRepo: "https://github.com/subarnoturja/StoryStacks-client",
+    serverRepo: "https://github.com/subarnoturja/StoryStacks-server",
   },
   {
-    title: "Task Management App",
+    title: "Trip Planner AI",
     image: project2,
     description:
-      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "An application for planning trips and vacation, with features such as suggesting visiting location, booking, and trip tracking.",
+    technologies: ["React", "TailwindCSS", "Gemini", "Firebase"],
+    liveLink: "https://storestacks.com",
+    clientRepo: "https://github.com/subarnoturja/StoryStacks-client",
+    serverRepo: "https://github.com/subarnoturja/StoryStacks-server",
   },
   {
-    title: "Portfolio Website",
+    title: "Quick Recipe",
     image: project3,
     description:
-      "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+      "A recipe suggesting website where recipes are giving for different dishes.",
+    technologies: ["React", "TailwindCSS", "Node.js", "MongoDB", "Firebase", "Express.js"],
+    liveLink: "https://storestacks.com",
+    clientRepo: "https://github.com/subarnoturja/StoryStacks-client",
+    serverRepo: "https://github.com/subarnoturja/StoryStacks-server",
   },
   {
-    title: "Blogging Platform",
+    title: "Easy Yoga",
     image: project4,
     description:
-      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
-    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+      "A yoga training website where you can buy yoga classes for yourself. Here are three different dashboards are available",
+    technologies: ["React", "TailwindCSS", "Node.js", "MongoDB", "Firebase", "Express.js"],
+    liveLink: "https://storestacks.com",
+    clientRepo: "https://github.com/subarnoturja/StoryStacks-client",
+    serverRepo: "https://github.com/subarnoturja/StoryStacks-server",
   },
 ];
 
