98a29d1ab6acaa231671677e2052fd5f4daf17c8
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-08 23:27:58 +0600

    motion added in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,22 @@
 import profilePic from "../assets/raviKumarProfile.webp";
+import { motion } from "framer-motion";
+
+const containerVariants = {
+  hidden: { opacity: 0, x: -100},
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+      staggerChildren: 0.5,
+    }
+  }
+}
+
+const childVariants = { 
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x:0, transition: { duration: 0.5 }}
+}
 
 const Hero = () => {
   return (
@@ -6,22 +24,37 @@ const Hero = () => {
       <div className="flex flex-wrap lg:flex-row-reverse">
         <div className="w-full lg:w-1/2">
           <div className="flex justify-center lg:p-8">
-            <img
+            <motion.img
               src={profilePic}
               alt="Subarno Ranjan Barua"
               className="border border-stone-900 rounded-3xl"
+              width={650}
+              height={650}
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
             />
           </div>
         </div>
         <div className="w-full lg:w-1/2">
-          <div className="flex flex-col items-center lg:items-start mt-10">
-            <h2 className="pb-2 text-4xl tracking-tighter lg:text-8xl">
+          <motion.div 
+          initial="hidden"
+          animate="visible"
+          variants={containerVariants}
+          className="flex flex-col items-center lg:items-start mt-10">
+            <motion.h2
+             variants={childVariants}
+            className="pb-2 text-4xl tracking-tighter lg:text-8xl">
               Subarno Ranjan Barua
-            </h2>
-            <span className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h2>
+            <motion.span
+            variants={childVariants}
+             className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent">
               Full Stack Developer
-            </span>
-            <p className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
+            </motion.span>
+            <motion.p 
+            variants={childVariants}
+            className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter">
               I am an aspiring full stack developer passionate about creating
               dynamic, user-friendly web experiences. I specialize in both
               frontend and backend development, utilizing modern tools like
@@ -31,10 +64,10 @@ const Hero = () => {
               updated with industry trends. I strive to bridge creativity and
               technology, delivering impactful solutions to real-world problems
               while collaborating and learning as I grow in the tech community.
-            </p>
-            <a href="/Resume.pdf" target="_blank" rel="noopener noreferrer" download className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">Download Resume
-            </a>
-          </div>
+            </motion.p>
+            <motion.a href="/Resume.pdf" target="_blank" rel="noopener noreferrer" download variants={childVariants} className="bg-white rounded-full p-4 text-sm text-stone-800 mb-10">Download Resume
+            </motion.a>
+          </motion.div>
         </div>
       </div>
     </div>
