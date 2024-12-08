1216f5563880f445eaa4ba0e23d53ef22e333f1e
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2024-12-09 00:14:10 +0600

    motion added in technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -3,38 +3,90 @@ import { FaNodeJs } from "react-icons/fa";
 import { RiReactjsLine, RiTailwindCssFill } from "react-icons/ri";
 import { SiExpress, SiMongodb } from "react-icons/si";
 import { TbBrandFirebase } from "react-icons/tb";
+import { motion } from "framer-motion"
 
+const iconVariants = (duration) => ({
+    initial: { y: -10 },
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        }
+    }
+})
 
 const Technologies = () => {
     return (
         <div className="pb-24">
-            <h2 className="my-20 text-center text-4xl">Technologies</h2>
-            <div className="flex flex-wrap items-center justify-center gap-4">
-                <div className="p-4">
+            <motion.h2
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1.5 }} 
+            className="my-20 text-center text-4xl">Technologies</motion.h2>
+            <motion.div
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100}}
+            transition={{ duration: 1.5 }}
+             className="flex flex-wrap items-center justify-center gap-4">
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(2.5)}>
                     <DiHtml5 className="text-7xl text-orange-600"></DiHtml5>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(3)}
+                className="p-4">
                     <DiCss3 className="text-7xl text-blue-400"></DiCss3>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(4)}
+                className="p-4">
                    <RiTailwindCssFill className="text-7xl text-blue-600"></RiTailwindCssFill>
-                </div>
-                <div>
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(3)}
+                className="p-4">
                     <RiReactjsLine className="text-7xl text-cyan-500"></RiReactjsLine>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(2)}
+                className="p-4">
                    <TbBrandFirebase className="text-7xl text-orange-400"></TbBrandFirebase>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(4)}
+                className="p-4">
                     <FaNodeJs className="text-7xl text-green-500"></FaNodeJs>
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(5)}
+                className="p-4">
                     <SiMongodb className="text-7xl text-green-600" />
-                </div>
-                <div className="p-4">
+                </motion.div>
+                <motion.div 
+                initial="initial"
+                animate="animate"
+                variants={iconVariants(3)}
+                className="p-4">
                     <SiExpress className="text-7xl text-white"></SiExpress>
-                </div>
-            </div>
+                </motion.div>
+            </motion.div>
         </div>
     );
 };
