75ba4d598f0f06958a9adab8864a44c53702d6bb
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-01-23 20:41:53 +0600

    updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,22 +1,21 @@
-import project1 from "../assets/projects/StoryStacks.png"
-import project2 from "../assets/projects/noteify.png";
-import project3 from "../assets/projects/project-3.webp";
-import project4 from "../assets/projects/project-3.webp";
+import noteify from "../assets/projects/noteify.png";
+import storyStacks from "../assets/projects/StoryStacks.png"
+import curePoint from "../assets/projects/curePoint.png";
 
 export const PROJECTS = [
   {
-    title: "StoreStacks",
-    image: project1,
+    title: "CurePoint",
+    image: curePoint,
     description:
-      "A fully functional book selling website with features like book listing, seller dashboard, and user authentication.",
-    technologies: ["React", "Node.js", "MongoDB", "Firebase", "Express.js"],
-    liveLink: "https://story-stacks.vercel.app/",
-    clientRepo: "https://github.com/subarnoturja/StoryStacks-client",
-    serverRepo: "https://github.com/subarnoturja/StoryStacks-server",
+      "A recipe suggesting website where recipes are giving for different dishes.",
+    technologies: ["React", "TailwindCSS", "Node.js", "MongoDB", "Firebase", "Express.js"],
+    liveLink: "https://storestacks.com",
+    clientRepo: "https://github.com/subarnoturja/curepoint-client",
+    serverRepo: "",
   },
   {
     title: "Noteify",
-    image: project2,
+    image: noteify,
     description:
       "Noteify is a web application designed to help users create, organize, and manage notes efficiently.",
     technologies: ["React", "TailwindCSS", "Node.js", "MongoDB"],
@@ -25,24 +24,14 @@ export const PROJECTS = [
     serverRepo: "https://github.com/subarnoturja/noteify-server",
   },
   {
-    title: "Quick Recipe",
-    image: project3,
+    title: "StoreStacks",
+    image: storyStacks,
     description:
-      "A recipe suggesting website where recipes are giving for different dishes.",
-    technologies: ["React", "TailwindCSS", "Node.js", "MongoDB", "Firebase", "Express.js"],
-    liveLink: "https://storestacks.com",
-    clientRepo: "",
-    serverRepo: "",
-  },
-  {
-    title: "Easy Yoga",
-    image: project4,
-    description:
-      "A yoga training website where you can buy yoga classes for yourself. Here are three different dashboards are available",
-    technologies: ["React", "TailwindCSS", "Node.js", "MongoDB", "Firebase", "Express.js"],
-    liveLink: "https://storestacks.com",
-    clientRepo: "",
-    serverRepo: "",
+      "A fully functional book selling website with features like book listing, seller dashboard, and user authentication.",
+    technologies: ["React", "Node.js", "MongoDB", "Firebase", "Express.js"],
+    liveLink: "https://story-stacks.vercel.app/",
+    clientRepo: "https://github.com/subarnoturja/StoryStacks-client",
+    serverRepo: "https://github.com/subarnoturja/StoryStacks-server",
   },
 ];
 
