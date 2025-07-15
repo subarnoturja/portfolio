467a9e815add34129ecf3a3e03ada53e21903d33
Subarno Ranjan Barua <subarnoturja15@gmail.com>
2025-07-16 00:41:52 +0600

    updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,37 +1,37 @@
-import noteify from "../assets/projects/noteify.png";
-import storyStacks from "../assets/projects/StoryStacks.png"
+import carRental from "../assets/projects/CarRental.png";
+import quickShow from "../assets/projects/QuickShow.png";
 import curePoint from "../assets/projects/curePoint.png"
 
 export const PROJECTS = [
   {
+    title: "CarRental",
+    image: carRental,
+    description:
+      "CarRental is an intuitive web platform that simplifies booking rental cars by connecting users with available vehicles in real time.",
+    technologies: ["React", "Node.js", "MongoDB", "Express.js"],
+    liveLink: "https://car-rental-client-six.vercel.app/",
+    clientRepo: "https://github.com/subarnoturja/CarRental-client",
+    serverRepo: "https://github.com/subarnoturja/CarRental-server",
+  },
+  {
+    title: "QuickShow",
+    image: quickShow,
+    description:
+      "QuickShow is an online movie ticket booking platform designed to seamlessly connect movie-goers with theaters.",
+    technologies: ["React", "Node.js", "MongoDB", "Express.js"],
+    liveLink: "https://quick-show-client-ruby.vercel.app/",
+    clientRepo: "https://github.com/subarnoturja/QuickShow-client",
+    serverRepo: "https://github.com/subarnoturja/QuickShow-server",
+  },
+  {
     title: "CurePoint",
     image: curePoint,
     description:
-      "CurePoint is a online doctor's appointment web platform which designed to connect patients with doctors.",
-    technologies: ["React", "TailwindCSS", "MongoDB", "Express.js"],
+      "CurePoint is an online doctor's appointment web platform designed to connect patients with doctors.",
+    technologies: ["React", "Tailwind", "MongoDB", "Express.js"],
     liveLink: "https://curepoint-client.vercel.app/",
     clientRepo: "https://github.com/subarnoturja/curepoint-client",
     serverRepo: "https://github.com/subarnoturja/curepoint-server",
-  },
-  {
-    title: "Noteify",
-    image: noteify,
-    description:
-      "Noteify is a web application designed to help users create, organize, and manage notes efficiently.",
-    technologies: ["React", "Express.js", "Node.js", "MongoDB"],
-    liveLink: "https://noteify-client.vercel.app/",
-    clientRepo: "https://github.com/subarnoturja/noteify-client",
-    serverRepo: "https://github.com/subarnoturja/noteify-server",
-  },
-  {
-    title: "StoreStacks",
-    image: storyStacks,
-    description:
-      "A fully functional book selling website with features like book listing, seller dashboard, and user authentication.",
-    technologies: ["React", "Node.js", "MongoDB", "Express.js"],
-    liveLink: "https://story-stacks.vercel.app/",
-    clientRepo: "https://github.com/subarnoturja/StoryStacks-client",
-    serverRepo: "https://github.com/subarnoturja/StoryStacks-server",
   },
 ];
 
